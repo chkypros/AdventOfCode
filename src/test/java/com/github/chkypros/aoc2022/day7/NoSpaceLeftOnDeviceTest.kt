713c9d49bc074b9d867d4dc4525448fc9565a47b
Kypros Chrysanthou <chkypros@gmail.com>
2022-12-08 03:43:59 +0200

    Day 7 No Space Left On Device

---
+++ src/test/java/com/github/chkypros/aoc2022/day7/NoSpaceLeftOnDeviceTest.kt
@@ -0,0 +1,24 @@
+package com.github.chkypros.aoc2022.day7
+
+import com.github.chkypros.aoc_template.AbstractSolutionTest
+import org.junit.Before
+import org.junit.Test
+
+class NoSpaceLeftOnDeviceTest : AbstractSolutionTest() {
+    @Before
+    fun setUp() {
+        solution = NoSpaceLeftOnDevice()
+        expectedAnswerPartOneSample = 95437
+        expectedAnswerPartTwoSample = 24933642
+    }
+
+    @Test
+    override fun solvePartOneSample() {
+        super.solvePartOneSample()
+    }
+
+    @Test
+    override fun solvePartTwoSample() {
+        super.solvePartTwoSample()
+    }
+}
