5ef88043d087fc32d6e5f2f607165dc2966bb1ae
Kypros Chrysanthou <chkypros@gmail.com>
2022-12-12 01:59:39 +0200

    Day 11 Monkey In The Middle Part One

---
+++ src/main/kotlin/com/github/chkypros/aoc2022/day11/MonkeyInTheMiddle.kt
@@ -0,0 +1,96 @@
+package com.github.chkypros.aoc2022.day11
+
+import com.github.chkypros.aoc_template.AbstractSolution
+import java.util.stream.Stream
+
+class MonkeyInTheMiddle: AbstractSolution() {
+    override fun solvePartOne(stream: Stream<String>): Long {
+        return solveForRoundsWithRelief(stream, 20)
+        { item -> item / 3 }
+    }
+
+    override fun solvePartTwo(stream: Stream<String>): Long {
+        return solveForRoundsWithRelief(stream, 1000)
+        { item -> item }
+    }
+
+    private fun solveForRoundsWithRelief(
+        stream: Stream<String>,
+        rounds: Long,
+        relief: (Long) -> Long
+    ): Long {
+        val monkeys = parseNotes(stream)
+
+        for (r in 0 until rounds) {
+            for (m in monkeys) {
+                while (m.items.isNotEmpty()) {
+                    var item = m.items.removeFirst()
+                    item = m.operation(item)
+                    item = relief(item)
+                    val nextMonkey = m.toMonkey[m.test(item)]
+                    monkeys[nextMonkey!!].items.add(item)
+                }
+            }
+        }
+
+        val monkeyInspections = monkeys.map { it.inspections.toLong() }.sorted().reversed()
+        return monkeyInspections[0] * monkeyInspections[1]
+    }
+
+    private fun parseNotes(stream: Stream<String>): List<Monkey> {
+        val descriptionsLine = stream.reduce { x, y -> "$x\n$y" }.get()
+        return descriptionsLine.split("\n\n")
+            .map { Monkey(it) }
+    }
+
+    class Monkey(description: String) {
+        val operation: (Long) -> Long
+        val test: (Long) -> Boolean
+        val toMonkey = HashMap<Boolean, Int>()
+
+        var items: MutableList<Long>
+        var inspections = 0
+
+        init {
+            val descriptionLines = description.split("\n")
+            // region Setup Starting Items
+            items = descriptionLines[1]
+                .substring(descriptionLines[1].indexOf(':') + 1)
+                .split(',', ' ')
+                .filter { it.isNotBlank() }
+                .map { it.toLong() }
+                .toMutableList()
+            // endregion
+
+            // region Setup Operation
+            val operationsLine = descriptionLines[2]
+                .substring(descriptionLines[2].indexOf(":") + 1)
+                .split(' ')
+                .filter { it.isNotBlank() }
+
+            val isAddition = operationsLine[3] == "+"
+            val isSecondArgumentOld = operationsLine[4] == "old"
+            operation = if (isAddition) {
+                { old ->
+                        inspections++
+                        old + if (isSecondArgumentOld) old else operationsLine[4].toLong()
+                }
+            } else {
+                { old ->
+                    inspections++
+                    old * if (isSecondArgumentOld) old else operationsLine[4].toLong()
+                }
+            }
+            // endregion
+
+            // region Setup test
+            test = { value -> 0L == value % descriptionLines[3].split(' ').filter { it.isNotBlank() }[3].toLong() }
+            // endregion
+
+            // region Setup Monkeys to throw
+            toMonkey[true] = descriptionLines[4].split(' ').filter { it.isNotBlank() }[5].toInt()
+            toMonkey[false] = descriptionLines[5].split(' ').filter { it.isNotBlank() }[5].toInt()
+            // endregion
+        }
+    }
+}
---
+++ src/test/kotlin/com/github/chkypros/aoc2022/day11/MonkeyInTheMiddleTest.kt
@@ -0,0 +1,27 @@
+package com.github.chkypros.aoc2022.day11
+
+import com.github.chkypros.aoc_template.AbstractSolutionTest
+import org.junit.Before
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class MonkeyInTheMiddleTest: AbstractSolutionTest() {
+
+    @Before
+    fun setUp() {
+        solution = MonkeyInTheMiddle()
+        expectedAnswerPartOneSample = 10605
+        expectedAnswerPartTwoSample = 2713310158
+    }
+
+    @Test
+    override fun solvePartOneSample() {
+        super.solvePartOneSample()
+    }
+
+    @Test
+    override fun solvePartTwoSample() {
+        super.solvePartTwoSample()
+    }
+}
