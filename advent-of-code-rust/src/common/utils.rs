ba294026e0121cb77e15263bc0faa686534e258d
Kypros.Chrysanthou <kypros.chrysanthou@amdocs.com>
2023-10-16 12:33:08 +0300

    [WIP] Rust-based module framework

--- advent-of-code-rust/src/common/utils.rs
+++ advent-of-code-rust/src/common/utils.rs
@@ -12,9 +12,9 @@ pub fn get_input(filepath: &str) -> String {
 
 pub fn get_labelled_input(filepath: &str, label: &str) -> String {
     let regex = Regex::new(r"src[/\\](aoc\d{4})[/\\](day\d{1,2})").unwrap();
-    let captures = regex.captures(filepath).unwrap();
-    let aoc = captures.get(1).unwrap().as_str();
-    let day = captures.get(2).unwrap().as_str();
+    let (_, [aoc, day]) =
+        regex.captures(filepath)
+        .map(|cap| cap.extract()).unwrap();
 
     let input_file = format!("../inputs/{aoc}/{day}/aoc-{day}{label}-input.txt");
     let input_content = read_lines(input_file.as_str());
