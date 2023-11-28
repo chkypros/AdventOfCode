936059af21fdfb6034ef03f2cb75422916c383ff
Kypros.Chrysanthou <kypros.chrysanthou@amdocs.com>
2023-11-28 09:22:20 +0200

    AOP 2022 - Day 03 Part One refactor

--- advent-of-code-rust/src/aoc2022/day3/rucksack_reorganization.rs
+++ advent-of-code-rust/src/aoc2022/day3/rucksack_reorganization.rs
@@ -31,27 +31,29 @@ impl solution::Solution for RucksackReorganization {
 }
 
 fn separate_compartments(input_content: &str) -> (HashSet<char>, HashSet<char>) {
-    let all_items_content:Vec<char> = input_content.chars().collect();
-    let half_index = all_items_content.len() / 2;
-    let items_compartment1 = all_items_content.iter().take(half_index).map(&char::to_owned).collect();
-    let items_compartment2 = all_items_content.iter().skip(half_index).map(&char::to_owned).collect();
+    let half_index = input_content.len() / 2;
+    let compartment_content = input_content.split_at(half_index);
+    let items_compartment1 = get_characters(compartment_content.0);
+    let items_compartment2 = get_characters(compartment_content.1);
     (items_compartment1, items_compartment2)
 }
 
+fn get_characters(content: &str) -> HashSet<char> {
+    content.chars().collect()
+}
+
 fn find_common_item(compartments: (HashSet<char>, HashSet<char>)) -> char {
-    let mut intersection = compartments.0.intersection(&compartments.1);
-    let Some(item) = intersection.next()
-        else {
-            panic!("Could not find intersection")
-        };
-    item.to_owned()
+    let Some(common_item) = compartments.0
+        .intersection(&compartments.1)
+        .next() else {panic!("Could not find common item between {compartments:?}")};
+    common_item.to_owned()
 }
 
 fn calculate_item_priority(item: char) -> i32 {
-    return match item {
-        'a'..='z' => 1 + (item as i32 - 'a' as i32),
-        'A'..='Z' => 27 + (item as i32 - 'A' as i32),
-        _ => panic!("Invalid item found!")
+    match item {
+        'a'..='z' => (item as i32) - ('a' as i32) + 1,
+        'A'..='Z' => (item as i32) - ('A' as i32) + 27,
+        _ => panic!("Got invalid item: {item}")
     }
 }
 
