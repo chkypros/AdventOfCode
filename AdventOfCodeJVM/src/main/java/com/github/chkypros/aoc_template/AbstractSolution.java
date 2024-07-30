22bc7cb652cbb93c03726c1a8985a18bfef7aea5
Kypros.Chrysanthou <kypros.chrysanthou@amdocs.com>
2024-07-30 10:26:15 +0300

    Simplifying AbstractSolution by removing duplication

--- AdventOfCodeJVM/src/main/java/com/github/chkypros/aoc_template/AbstractSolution.java
+++ AdventOfCodeJVM/src/main/java/com/github/chkypros/aoc_template/AbstractSolution.java
@@ -2,23 +2,19 @@ package com.github.chkypros.aoc_template;
 
 import com.github.chkypros.aoc_common.Utils;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Optional;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
 public abstract class AbstractSolution {
+
     public void solve() throws Exception {
         final Path inputFile = Utils.getInputFilePath(this);
 
-        try (Stream<String> stream = Files.lines(inputFile)) {
-            solvePart(stream, this::solvePartOne, "part one");
-        }
-
-        try (Stream<String> stream = Files.lines(inputFile)) {
-            solvePart(stream, this::solvePartTwo, "part two");
-        }
+        solvePart(inputFile, this::solvePartOne, "part one");
+        solvePart(inputFile, this::solvePartTwo, "part two");
     }
 
     public Object solvePartOne(Stream<String> stream) {
@@ -29,8 +25,10 @@ public abstract class AbstractSolution {
         return null;
     }
 
-    private void solvePart(final Stream<String> stream, final Function<Stream<String>, Object> function, final String label) {
-        Optional.ofNullable(function.apply(stream))
-                .ifPresent(answer -> System.out.println(label + " answer = " + answer));
+    private void solvePart(final Path inputFilePath, final Function<Stream<String>, Object> function, final String label) throws IOException {
+        try (Stream<String> stream = Files.lines(inputFilePath)) {
+            Object answer = function.apply(stream);
+            if (answer != null) System.out.println(label + " answer = " + answer);
+        }
     }
 }
