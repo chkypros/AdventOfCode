5e1aa1585794daafb3f5e7d39c27caba8fff0db1
Kypros Chrysanthou <chkypros@gmail.com>
2024-10-19 17:55:13 +0300

    wip

--- AdventOfCodePython/test/aoc_common/test_utils.py
+++ AdventOfCodePython/test/aoc_common/test_utils.py
@@ -3,8 +3,8 @@ import os
 def get_input(reference: object) -> str:
     return get_input_path(reference)
 
-def get_input_path(reference: object) -> str:
+def get_input_path(reference: object, label: str = "") -> str:
     module = reference.__module__
     path_items = module.split(".")[1:-1]
-    path_items.append(f"aoc-{path_items[-1]}-input.txt")
+    path_items.append(f"aoc-{path_items[-1]}{label}-input.txt")
     return os.path.join(*path_items)
