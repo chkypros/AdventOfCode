ca2878dd22e541540cb32da694568cf3d63e8a50
Kypros.Chrysanthou <kypros.chrysanthou@amdocs.com>
2024-10-21 11:21:21 +0300

    renaming variable so as not to hide any built-in functions

--- AdventOfCodePython/test/aoc_template/solution_test.py
+++ AdventOfCodePython/test/aoc_template/solution_test.py
@@ -1,12 +1,13 @@
 import unittest
-import test.aoc_common.test_utils as test_utils
 
+import test.aoc_common.test_utils as test_utils
 from src.aoc_template.solution import AbstractSolution
+
 
 class AbstractSolutionTest(unittest.TestCase):
     solution: AbstractSolution
 
     def test_part_one(self):
         if hasattr(self, 'EXPECTED_ANSWER_PART_ONE'):
-            input = test_utils.get_input(self)
-            self.assertEqual(self.EXPECTED_ANSWER_PART_ONE, self.solution.solve_part_one(input))
+            input_content = test_utils.get_input(self)
+            self.assertEqual(self.EXPECTED_ANSWER_PART_ONE, self.solution.solve_part_one(input_content))
