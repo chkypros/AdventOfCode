c4127859deccbb1d1974853df30e5c387b83212e
Kypros.Chrysanthou <kypros.chrysanthou@amdocs.com>
2024-10-24 17:14:02 +0300

    marking abstract methods explicitly as NotImplemented

--- AdventOfCodePython/src/aoc_template/solution.py
+++ AdventOfCodePython/src/aoc_template/solution.py
@@ -1,7 +1,7 @@
 
-class AbstractSolution():
+class AbstractSolution:
     def solve_part_one(self, input_lines: list[str]) -> str:
-        pass
+        return NotImplemented
 
     def solve_part_two(self, input_lines: list[str]) -> str:
-        pass
+        return NotImplemented
