edb0e7a42c5fb38aac1c663a55627689a0f84d0e
Kypros Chrysanthou <chkypros@gmail.com>
2024-10-31 23:03:20 +0200

    AOP 2023 - Day 03 Implemented part two

--- AdventOfCodePython/src/aoc2023/day1/trebuchet.py
+++ AdventOfCodePython/src/aoc2023/day1/trebuchet.py
@@ -2,12 +2,34 @@ import re
 
 import src.aoc_template.solution as solution
 
+VALID_DIGIT_WORDS = "one|two|three|four|five|six|seven|eight|nine"
 
-def _get_calibration_value(line: str) -> int:
+
+def _solve(input_lines: list[str], get_calibration_value) -> str:
+    calibration_values = map(lambda line : get_calibration_value(line), input_lines)
+    return str(sum(calibration_values))
+
+
+def _get_digit(text):
+    if len(text) == 1: return text
+    word_list = VALID_DIGIT_WORDS.split("|")
+    value = word_list.index(text) + 1
+    return str(value)
+
+def _get_calibration_value_p1(line: str) -> int:
     characters = re.findall("[\d]", line)
     return int(characters[0] + characters[-1])
 
+def _get_calibration_value_p2(line: str) -> int:
+    characters = re.findall(f'(?=(\d|{VALID_DIGIT_WORDS}))', line)
+
+    digit1 = _get_digit(characters[0])
+    digit2 = _get_digit(characters[-1])
+    return int(digit1 + digit2)
+
 class Trebuchet(solution.AbstractSolution):
     def solve_part_one(self, input_lines: list[str]) -> str:
-        calibration_values = map(lambda line : _get_calibration_value(line), input_lines)
-        return str(sum(calibration_values))
+        return _solve(input_lines, _get_calibration_value_p1)
+
+    def solve_part_two(self, input_lines: list[str]) -> str:
+        return _solve(input_lines, _get_calibration_value_p2)
--- AdventOfCodePython/test/aoc2023/day1/test_trebuchet.py
+++ AdventOfCodePython/test/aoc2023/day1/test_trebuchet.py
@@ -6,8 +6,14 @@ from src.aoc2023.day1.trebuchet import Trebuchet
 class TrebuchetTest(solution_test.AbstractSolutionTest):
     EXPECTED_ANSWER_PART_ONE_SAMPLE_ONE = "142"
     EXPECTED_ANSWER_PART_ONE = "55108"
+    EXPECTED_ANSWER_PART_TWO_SAMPLE_TWO = "281"
+    EXPECTED_ANSWER_PART_TWO = "56324"
     solution = Trebuchet()
 
     def test_part_one_sample(self):
         input_lines = test_utils.get_input_lines(self, "-sample1")
         self.assertEqual(self.EXPECTED_ANSWER_PART_ONE_SAMPLE_ONE, self.solution.solve_part_one(input_lines))
+
+    def test_part_two_sample(self):
+        input_lines = test_utils.get_input_lines(self, "-sample2")
+        self.assertEqual(self.EXPECTED_ANSWER_PART_TWO_SAMPLE_TWO, self.solution.solve_part_two(input_lines))
