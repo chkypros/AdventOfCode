9dee8f214007ebff677ca039aea00ce874461b5a
Kypros.Chrysanthou <kypros.chrysanthou@amdocs.com>
2024-10-31 16:02:34 +0200

    AOP 2023 - Day 03 Refactor

--- AdventOfCodePython/src/aoc2023/day1/trebuchet.py
+++ AdventOfCodePython/src/aoc2023/day1/trebuchet.py
@@ -1,11 +1,13 @@
-import src.aoc_template.solution as solution
 import re
+
+import src.aoc_template.solution as solution
+
+
+def _get_calibration_value(line: str) -> int:
+    characters = re.findall("[\d]", line)
+    return int(characters[0] + characters[-1])
 
 class Trebuchet(solution.AbstractSolution):
     def solve_part_one(self, input_lines: list[str]) -> str:
-        calibration_values = map(lambda line : self.__get_calibration_value(line), input_lines)
+        calibration_values = map(lambda line : _get_calibration_value(line), input_lines)
         return str(sum(calibration_values))
-
-    def __get_calibration_value(self, line: str) -> int:
-        characters = re.findall("[0-9]", line)
-        return int(characters[0] + characters[-1])
