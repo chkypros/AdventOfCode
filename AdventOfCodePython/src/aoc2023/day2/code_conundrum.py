0f4ac58cee816e8761cac39553e3df30b7a479a6
Kypros Chrysanthou <chkypros@gmail.com>
2024-11-03 00:42:13 +0200

    AOP 2023 - Day 02 Implemented part two

--- AdventOfCodePython/src/aoc2023/day2/code_conundrum.py
+++ AdventOfCodePython/src/aoc2023/day2/code_conundrum.py
@@ -32,9 +32,22 @@ def _part_one_filter_line(game: tuple[int, list[tuple[int, int, int]]]) -> bool:
                 return False
     return True
 
+def _part_two_get_game_power(game: tuple[int, list[tuple[int, int, int]]]) -> int:
+    min_set = reduce(lambda m, s: (
+        max(m[0], s[0]),
+        max(m[1], s[1]),
+        max(m[2], s[2])
+    ), game[1], (0, 0, 0))
+    return reduce(lambda a,b: a * b, min_set)
+
 class CodeConundrum(solution.AbstractSolution):
     def solve_part_one(self, input_lines: list[str]) -> str:
         game_lines = map(_parse_line, input_lines)
         filtered_game_lines = filter(_part_one_filter_line, game_lines)
         game_id_sum = reduce(lambda sum, element: sum + element[0], filtered_game_lines, 0)
         return str(game_id_sum)
+
+    def solve_part_two(self, input_lines: list[str]) -> str:
+        game_lines = map(_parse_line, input_lines)
+        game_powers = map(_part_two_get_game_power, game_lines)
+        return str(sum(game_powers))
--- AdventOfCodePython/test/aoc2023/day2/test_code_conundrum.py
+++ AdventOfCodePython/test/aoc2023/day2/test_code_conundrum.py
@@ -5,4 +5,6 @@ from src.aoc2023.day2.code_conundrum import CodeConundrum
 class CodeConundrumTest(solution_test.AbstractSolutionTest):
     EXPECTED_ANSWER_PART_ONE_SAMPLE = "8"
     EXPECTED_ANSWER_PART_ONE = "2377"
+    EXPECTED_ANSWER_PART_TWO_SAMPLE = "2286"
+    EXPECTED_ANSWER_PART_TWO = "71220"
     solution = CodeConundrum()
