68c3d568329c8ccc3fb17800d0b377ced1caff72
Kypros Chrysanthou <chkypros@gmail.com>
2024-11-06 09:04:34 +0200

    AOP 2023 - Day 03 Implemented part two

--- AdventOfCodePython/src/aoc2023/day3/gear_ratios.py
+++ AdventOfCodePython/src/aoc2023/day3/gear_ratios.py
@@ -48,8 +48,64 @@ def _is_symbol(schematic: list[str], row: int, column: int) -> bool:
     c = schematic[row][column]
     return not c.isnumeric() and c != "."
 
+
+def _get_gear_ratios(schematic: list[str]) -> list[int]:
+    symbols: dict[tuple[str, int, int], list[int]] = {}
+
+    for row in range(len(schematic)):
+        start = None
+        end = None
+        adjacent_symbols = set()
+        for column in range(len(schematic[row])):
+            c = schematic[row][column]
+            if c.isnumeric():
+                if start is None:
+                    start = column
+                adjacent_symbols.update(_get_adjacent_symbols(schematic, row, column))
+                if column == len(schematic[row]) - 1:
+                    end = column + 1
+            else:
+                if start is not None:
+                    end = column
+
+            if end is not None:
+                number = schematic[row][start:end]
+                [_add_number_to_symbol(number, s, symbols) for s in adjacent_symbols]
+                start = None
+                end = None
+                adjacent_symbols = set()
+
+    return [v[0]*v[1] for (k, v) in symbols.items() if k[0] == "*" and len(v) == 2]
+
+
+def _add_number_to_symbol(number, s, symbols):
+    if s not in symbols:
+        symbols[s] = []
+    return symbols[s].append(int(number))
+
+def _get_adjacent_symbols(schematic: list[str], row: int, column: int) -> set[tuple[str, int, int]]:
+    adjacent_symbols = set()
+
+    for r in range(row - 1, row + 2):
+        for c in range(column - 1, column + 2):
+            if r != row or c != column:
+                symbol = _find_symbol(schematic, r, c)
+                if symbol is not None:
+                    adjacent_symbols.add(symbol)
+
+    return adjacent_symbols
+
+
+def _find_symbol(schematic: list[iter], row: int, column: int) -> tuple[str, int, int] | None:
+    if (_is_symbol(schematic, row, column)):
+        return (schematic[row][column], row, column)
+    return None
+
 class GearRatios(solution.AbstractSolution):
     def solve_part_one(self, input_lines: list[str]) -> str:
-        schematic = input_lines
-        numbers = _get_symbol_adjacent_numbers(schematic)
+        numbers = _get_symbol_adjacent_numbers(input_lines)
         return str(sum(numbers))
+
+    def solve_part_two(self, input_lines: list[str]) -> str:
+        gear_ratios = _get_gear_ratios(input_lines)
+        return str(sum(gear_ratios))
--- AdventOfCodePython/test/aoc2023/day3/test_gear_ratios.py
+++ AdventOfCodePython/test/aoc2023/day3/test_gear_ratios.py
@@ -5,4 +5,6 @@ from src.aoc2023.day3.gear_ratios import GearRatios
 class GearRatiosTest(solution_test.AbstractSolutionTest):
     EXPECTED_ANSWER_PART_ONE_SAMPLE = "4361"
     EXPECTED_ANSWER_PART_ONE = "533784"
+    EXPECTED_ANSWER_PART_TWO_SAMPLE = "467835"
+    EXPECTED_ANSWER_PART_TWO = "78826761"
     solution = GearRatios()
