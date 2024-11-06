5cec224a83b4a98b9b937187244f55a48539b8b9
Kypros Chrysanthou <chkypros@gmail.com>
2024-11-07 00:29:06 +0200

    AOP 2023 - Day 04 Implemented part one - WIP

--- AdventOfCodePython/src/aoc2023/day4/scratchcards.py
+++ AdventOfCodePython/src/aoc2023/day4/scratchcards.py
@@ -1,6 +1,22 @@
 
 import src.aoc_template.solution as solution
 
+
+def _parse_card_info(input_line: str) -> tuple[str, list[str], list[str]]:
+    colon_index = input_line.index(":")
+    pipe_index = input_line.index("|")
+    return (
+        input_line[:colon_index],
+        input_line[colon_index + 2:pipe_index - 1].split(),
+        input_line[pipe_index + 2:].split()
+    )
+
+def _get_winning_numbers(card_info: tuple[str, list[str], list[str]]) -> list[str]:
+    return [number for number in card_info[1] if number in card_info[2]]
+
 class Scratchcards(solution.AbstractSolution):
     def solve_part_one(self, input_lines: list[str]) -> str:
-        pass
+        card_info = [_parse_card_info(input_line) for input_line in input_lines]
+        [len(card_info) for card_info in card_info]
+
+        return card_info[0]
