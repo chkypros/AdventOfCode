d497c3d21a379d6c6071fae19185c7953234b8e1
Kypros Chrysanthou <chkypros@gmail.com>
2024-11-10 23:18:53 +0200

    AOP 2023 - Day 06 Implemented part two

--- AdventOfCodePython/src/aoc2023/day6/wait_for_it.py
+++ AdventOfCodePython/src/aoc2023/day6/wait_for_it.py
@@ -8,6 +8,11 @@ def _parse_race_info(input_lines: list[str]) -> list[tuple[int, int]]:
     distances = input_lines[1].split()[1:]
     return [(int(times[i]), int(distances[i])) for i in range(len(times))]
 
+def _parse_race_info_part_two(input_lines: list[str]) -> list[tuple[int, int]]:
+    times = str.join('', input_lines[0].split()[1:])
+    distances = str.join('', input_lines[1].split()[1:])
+    return [(int(times), int(distances))]
+
 
 def _travel_with_hold(hold_time, total_time):
     return (total_time - hold_time) * hold_time
@@ -35,6 +40,12 @@ def _find_win_range(time: int, distance: int):
 class WaitForIt(solution.AbstractSolution):
     def solve_part_one(self, input_lines: list[str]) -> str:
         race_infos = _parse_race_info(input_lines)
+        win_range_infos = [_find_win_range(*race_info) for race_info in race_infos]
+        result = reduce(lambda acc, range: acc * (range[1] + 1 - range[0]), win_range_infos, 1)
+        return str(result)
+
+    def solve_part_two(self, input_lines: list[str]) -> str:
+        race_infos = _parse_race_info_part_two(input_lines)
         win_range_infos = [_find_win_range(*race_info) for race_info in race_infos]
         result = reduce(lambda acc, range: acc * (range[1] + 1 - range[0]), win_range_infos, 1)
         return str(result)
--- AdventOfCodePython/test/aoc2023/day6/test_wait_for_it.py
+++ AdventOfCodePython/test/aoc2023/day6/test_wait_for_it.py
@@ -5,4 +5,6 @@ from src.aoc2023.day6.wait_for_it import WaitForIt
 class WaitForItTest(solution_test.AbstractSolutionTest):
     EXPECTED_ANSWER_PART_ONE_SAMPLE = "288"
     EXPECTED_ANSWER_PART_ONE = "1159152"
+    EXPECTED_ANSWER_PART_TWO_SAMPLE = "71503"
+    EXPECTED_ANSWER_PART_TWO = "41513103"
     solution = WaitForIt()
