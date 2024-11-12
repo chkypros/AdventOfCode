f2c07f7b40164059d11403f038edd9e6be36a8ca
Kypros Chrysanthou <chkypros@gmail.com>
2024-11-12 21:35:16 +0200

    Python framework - Extract test running template method

--- AdventOfCodePython/test/aoc_template/solution_test.py
+++ AdventOfCodePython/test/aoc_template/solution_test.py
@@ -8,29 +8,20 @@ class AbstractSolutionTest(unittest.TestCase):
     solution: AbstractSolution
 
     def test_part_one(self):
-        if hasattr(self, 'EXPECTED_ANSWER_PART_ONE'):
-            input_lines = test_utils.get_input_lines(self)
-            self.assertEqual(self.EXPECTED_ANSWER_PART_ONE, self.solution.solve_part_one(input_lines))
-        else:
-            self.skipTest("Not implemented yet")
+        self._run_test(self.solution.solve_part_one, 'EXPECTED_ANSWER_PART_ONE')
 
     def test_part_one_sample(self):
-        if hasattr(self, 'EXPECTED_ANSWER_PART_ONE_SAMPLE'):
-            input_lines = test_utils.get_input_lines(self, '-sample')
-            self.assertEqual(self.EXPECTED_ANSWER_PART_ONE_SAMPLE, self.solution.solve_part_one(input_lines))
-        else:
-            self.skipTest("Not implemented yet")
+        self._run_test(self.solution.solve_part_one, 'EXPECTED_ANSWER_PART_ONE_SAMPLE', '-sample')
 
     def test_part_two(self):
-        if hasattr(self, 'EXPECTED_ANSWER_PART_TWO'):
-            input_lines = test_utils.get_input_lines(self)
-            self.assertEqual(self.EXPECTED_ANSWER_PART_TWO, self.solution.solve_part_two(input_lines))
-        else:
-            self.skipTest("Not implemented yet")
+        self._run_test(self.solution.solve_part_two, 'EXPECTED_ANSWER_PART_TWO')
 
     def test_part_two_sample(self):
-        if hasattr(self, 'EXPECTED_ANSWER_PART_TWO_SAMPLE'):
-            input_lines = test_utils.get_input_lines(self, '-sample')
-            self.assertEqual(self.EXPECTED_ANSWER_PART_TWO_SAMPLE, self.solution.solve_part_two(input_lines))
+        self._run_test(self.solution.solve_part_two, 'EXPECTED_ANSWER_PART_TWO_SAMPLE', '-sample')
+
+    def _run_test(self, method, attribute, label = ""):
+        if hasattr(self, attribute):
+            input_lines = test_utils.get_input_lines(self, label)
+            self.assertEqual(self.__getattribute__(attribute), method(input_lines))
         else:
             self.skipTest("Not implemented yet")
