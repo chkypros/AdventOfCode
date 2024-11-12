37fc9247dbc2e547eb29815b5c4be5568c155179
Kypros Chrysanthou <chkypros@gmail.com>
2024-11-12 23:44:07 +0200

    AOP 2023 - Day 07 Implemented part two

--- AdventOfCodePython/src/aoc2023/day7/camel_cards.py
+++ AdventOfCodePython/src/aoc2023/day7/camel_cards.py
@@ -2,6 +2,9 @@ from enum import IntEnum
 
 import src.aoc_template.solution as solution
 
+CARDS_IN_HAND = 5
+JOKER_VALUE = 0
+
 class HandType(IntEnum):
     FIVE_OF_A_KIND = 20,
     FOUR_OF_A_KIND = 19,
@@ -13,14 +16,21 @@ class HandType(IntEnum):
 
 SPECIAL_CARD_VALUES = {'A': 14, 'K': 13, 'Q': 12, 'J': 11, 'T': 10}
 
-def _get_card_value(card: str) -> int:
+def _get_card_value(card: str, j_override: int | None = None) -> int:
+    if card == 'J' and j_override is not None:
+        return j_override
     if card in SPECIAL_CARD_VALUES:
         return SPECIAL_CARD_VALUES[card]
     else:
         return int(card)
 
 def _get_card_counts(card_values: list[int]) -> dict[int, int]:
-    return {card:card_values.count(card) for card in set(card_values)}
+    card_counts = {card: card_values.count(card) for card in set(card_values)}
+    if JOKER_VALUE in card_counts and card_counts[JOKER_VALUE] != CARDS_IN_HAND:
+        joker_count = card_counts.pop(JOKER_VALUE)
+        max_card = max(card_counts, key=card_counts.get)
+        card_counts[max_card] += joker_count
+    return card_counts
 
 def _get_hand_value(card_values: list[int]):
     card_counts = _get_card_counts(card_values)
@@ -41,8 +51,8 @@ def _get_hand_value(card_values: list[int]):
     else:
         return HandType.HIGH_CARD
 
-def _parse_input(input_line: str) -> tuple[HandType, list[int], int]:
-    card_values = [_get_card_value(card) for card in input_line.split()[0]]
+def _parse_input(input_line: str, j_override: int | None = None) -> tuple[HandType, list[int], int]:
+    card_values = [_get_card_value(card, j_override) for card in input_line.split()[0]]
     hand_type_value = _get_hand_value(card_values)
     bid = int(input_line.split()[1])
     return hand_type_value, card_values, bid
@@ -50,6 +60,12 @@ def _parse_input(input_line: str) -> tuple[HandType, list[int], int]:
 class CamelCards(solution.AbstractSolution):
     def solve_part_one(self, input_lines: list[str]) -> str:
         hand_infos = [_parse_input(input_line) for input_line in input_lines]
+        hand_infos.sort(key=lambda i: (i[0], i[1]))
+        total_winnings = sum([hand_infos[i][2] * (i + 1) for i in range(len(hand_infos))])
+        return str(total_winnings)
+
+    def solve_part_two(self, input_lines: list[str]) -> str:
+        hand_infos = [_parse_input(input_line, JOKER_VALUE) for input_line in input_lines]
         hand_infos.sort(key=lambda i: (i[0], i[1]))
         total_winnings = sum([hand_infos[i][2] * (i + 1) for i in range(len(hand_infos))])
         return str(total_winnings)
--- AdventOfCodePython/test/aoc2023/day7/test_camel_cards.py
+++ AdventOfCodePython/test/aoc2023/day7/test_camel_cards.py
@@ -5,4 +5,6 @@ from src.aoc2023.day7.camel_cards import CamelCards
 class CamelCardsTest(solution_test.AbstractSolutionTest):
     EXPECTED_ANSWER_PART_ONE_SAMPLE = "6440"
     EXPECTED_ANSWER_PART_ONE = "246912307"
+    EXPECTED_ANSWER_PART_TWO_SAMPLE = "5905"
+    EXPECTED_ANSWER_PART_TWO = "246894760"
     solution = CamelCards()
