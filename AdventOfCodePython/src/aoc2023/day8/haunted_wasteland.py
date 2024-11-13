9c33abcd56afd58231338113818eb9ea31211d92
Kypros Chrysanthou <chkypros@gmail.com>
2024-11-13 22:54:07 +0200

    AOP 2023 - Day 08 Implemented part one

--- AdventOfCodePython/src/aoc2023/day8/haunted_wasteland.py
+++ AdventOfCodePython/src/aoc2023/day8/haunted_wasteland.py
@@ -1,6 +1,25 @@
-
+import re
 import src.aoc_template.solution as solution
+
+def _map_network_node(line: str) -> tuple[str , tuple[str, str]]:
+    result = re.search("([A-Z]{3}) = \\(([A-Z]{3}), ([A-Z]{3})\\)", line)
+    if result is None: raise IOError
+    return result.group(1), (result.group(2), result.group(3))
+
+def _parse_input(input_lines: list[str]) -> tuple[list[int], dict[str, tuple[str, str]]]:
+    sequence = [1 if instruction == 'R' else 0 for instruction in input_lines[0]]
+    network = dict([_map_network_node(line) for line in input_lines[2:]])
+    return sequence, network
 
 class HauntedWasteland(solution.AbstractSolution):
     def solve_part_one(self, input_lines: list[str]) -> object:
-        pass
+        sequence, network = _parse_input(input_lines)
+        steps = 0
+        node = 'AAA'
+        while True:
+            index = steps % len(sequence)
+            steps += 1
+            node = network[node][sequence[index]]
+            if node == 'ZZZ': break
+
+        return steps
--- AdventOfCodePython/test/aoc2023/day8/test_haunted_wasteland.py
+++ AdventOfCodePython/test/aoc2023/day8/test_haunted_wasteland.py
@@ -5,7 +5,9 @@ from src.aoc2023.day8.haunted_wasteland import HauntedWasteland
 class HauntedWastelandTest(solution_test.AbstractSolutionTest):
     EXPECTED_ANSWER_PART_ONE_SAMPLE = "2"
     EXPECTED_ANSWER_PART_ONE_SAMPLE_TWO = "6"
+    EXPECTED_ANSWER_PART_ONE = "16271"
     solution = HauntedWasteland()
 
     def test_part_one_sample_two(self):
         self._run_test(self.solution.solve_part_one, "EXPECTED_ANSWER_PART_ONE_SAMPLE_TWO", "sample2")
+        self._run_test(self.solution.solve_part_one, "EXPECTED_ANSWER_PART_ONE_SAMPLE_TWO", "-sample2")
