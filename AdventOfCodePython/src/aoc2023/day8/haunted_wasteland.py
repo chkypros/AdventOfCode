c24ed37e524d9b32edc0ca95d9bb32ab908e61e0
Kypros Chrysanthou <chkypros@gmail.com>
2024-11-16 00:02:39 +0200

    AOP 2023 - Day 08 Implemented part two

--- AdventOfCodePython/src/aoc2023/day8/haunted_wasteland.py
+++ AdventOfCodePython/src/aoc2023/day8/haunted_wasteland.py
@@ -1,3 +1,4 @@
+import math
 import re
 import src.aoc_template.solution as solution
 
@@ -11,18 +12,26 @@ def _parse_input(input_lines: list[str]) -> tuple[list[int], dict[str, tuple[str
     network = dict([_map_network_node(line) for line in input_lines[2:]])
     return sequence, network
 
+def _find_starting_nodes(nodes: list[str]) -> list[str]:
+    return list(filter(lambda n: n.endswith('A'), nodes))
+
+def _steps_until_z_ending(network, node, sequence) -> int:
+    steps = 0
+    while True:
+        index = steps % len(sequence)
+        steps += 1
+        node = network[node][sequence[index]]
+        if node.endswith('Z'): break
+    return steps
+
 class HauntedWasteland(solution.AbstractSolution):
+
     def solve_part_one(self, input_lines: list[str]) -> object:
         sequence, network = _parse_input(input_lines)
-        steps = 0
-        node = 'AAA'
-        while True:
-            index = steps % len(sequence)
-            steps += 1
-            node = network[node][sequence[index]]
-            if node == 'ZZZ': break
-
-        return steps
+        return _steps_until_z_ending(network, 'AAA', sequence)
 
     def solve_part_two(self, input_lines: list[str]) -> object:
-        pass
+        sequence, network = _parse_input(input_lines)
+        node_paths = _find_starting_nodes(list(network.keys()))
+        steps_to_z = [_steps_until_z_ending(network, node, sequence) for node in node_paths]
+        return math.lcm(*steps_to_z)
--- AdventOfCodePython/test/aoc2023/day8/test_haunted_wasteland.py
+++ AdventOfCodePython/test/aoc2023/day8/test_haunted_wasteland.py
@@ -7,10 +7,11 @@ class HauntedWastelandTest(solution_test.AbstractSolutionTest):
     EXPECTED_ANSWER_PART_ONE_SAMPLE_TWO = "6"
     EXPECTED_ANSWER_PART_ONE = "16271"
     EXPECTED_ANSWER_PART_TWO_SAMPLE_THREE = "6"
+    EXPECTED_ANSWER_PART_TWO = "14265111103729"
     solution = HauntedWasteland()
 
     def test_part_one_sample_two(self):
         self._run_test(self.solution.solve_part_one, "EXPECTED_ANSWER_PART_ONE_SAMPLE_TWO", "-sample2")
 
     def test_part_two_sample_three(self):
-        self._run_test(self.solution.solve_part_one, "EXPECTED_ANSWER_PART_TWO_SAMPLE_THREE", "-sample3")
+        self._run_test(self.solution.solve_part_two, "EXPECTED_ANSWER_PART_TWO_SAMPLE_THREE", "-sample3")
