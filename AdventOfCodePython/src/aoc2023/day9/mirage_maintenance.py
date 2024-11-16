7c727961e2e522bbf03f80f246f33859a1c53040
Kypros Chrysanthou <chkypros@gmail.com>
2024-11-16 15:53:11 +0200

    AOP 2023 - Day 09 Implemented part two

--- AdventOfCodePython/src/aoc2023/day9/mirage_maintenance.py
+++ AdventOfCodePython/src/aoc2023/day9/mirage_maintenance.py
@@ -17,6 +17,12 @@ def _calculate_sequence_stack(history: list[int]) -> list[list[int]]:
 def _extrapolate_next_value(sequence_stack: list[list[int]]) -> int:
     return sum([sequence[-1] for sequence in sequence_stack])
 
+def _extrapolate_prev_value(sequence_stack: list[list[int]]) -> int:
+    prev_value = 0
+    for i in reversed(range(0, len(sequence_stack))):
+        prev_value = sequence_stack[i][0] - prev_value
+    return prev_value
+
 class MirageMaintenance(solution.AbstractSolution):
 
     def solve_part_one(self, input_lines: list[str]) -> object:
@@ -24,3 +30,9 @@ class MirageMaintenance(solution.AbstractSolution):
         sequence_stacks = [_calculate_sequence_stack(history) for history in histories]
         next_values = [_extrapolate_next_value(sequence_stack) for sequence_stack in sequence_stacks]
         return sum(next_values)
+
+    def solve_part_two(self, input_lines: list[str]) -> object:
+        histories = [_parse_history(line) for line in input_lines]
+        sequence_stacks = [_calculate_sequence_stack(history) for history in histories]
+        prev_values = [_extrapolate_prev_value(sequence_stack) for sequence_stack in sequence_stacks]
+        return sum(prev_values)
--- AdventOfCodePython/test/aoc2023/day9/test_mirage_maintenance.py
+++ AdventOfCodePython/test/aoc2023/day9/test_mirage_maintenance.py
@@ -4,5 +4,7 @@ from src.aoc2023.day9.mirage_maintenance import MirageMaintenance
 
 class MirageMaintenanceTest(solution_test.AbstractSolutionTest):
     EXPECTED_ANSWER_PART_ONE_SAMPLE = "114"
-    EXPECTED_ANSWER_PART_ONE = "0"
+    EXPECTED_ANSWER_PART_ONE = "1980437560"
+    EXPECTED_ANSWER_PART_TWO_SAMPLE = "2"
+    EXPECTED_ANSWER_PART_TWO = "977"
     solution = MirageMaintenance()
