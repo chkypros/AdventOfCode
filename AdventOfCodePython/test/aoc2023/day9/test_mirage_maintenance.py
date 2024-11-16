63cb51dabf7f7979f923db68b55a934920940c5e
Kypros Chrysanthou <chkypros@gmail.com>
2024-11-16 14:51:41 +0200

    AOP 2023 - Day 09 Setup

---
+++ AdventOfCodePython/test/aoc2023/day9/test_mirage_maintenance.py
@@ -0,0 +1,7 @@
+import test.aoc_template.solution_test as solution_test
+
+from src.aoc2023.day9.mirage_maintenance import MirageMaintenance
+
+class MirageMaintenanceTest(solution_test.AbstractSolutionTest):
+    EXPECTED_ANSWER_PART_ONE_SAMPLE = "114"
+    solution = MirageMaintenance()
