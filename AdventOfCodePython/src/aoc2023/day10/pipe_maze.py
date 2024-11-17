ed49be10a3f1e66c38e6cf7b8e5bde01f2a24e60
Kypros Chrysanthou <chkypros@gmail.com>
2024-11-17 23:47:19 +0200

    AOP 2023 - Day 10 WIP

--- AdventOfCodePython/src/aoc2023/day10/pipe_maze.py
+++ AdventOfCodePython/src/aoc2023/day10/pipe_maze.py
@@ -1,16 +1,16 @@
 import src.aoc_template.solution as solution
 from aoc2023.day10.tile_graph import TileGraph, Tile, TileSide, PipeTile, GroundTile, StartingTile
 
-def _get_tile(tile_str: str) -> Tile:
+def _get_tile(tile_str: str, position: tuple[int, int]) -> Tile:
     match tile_str:
-        case '|': return PipeTile([TileSide.NORTH, TileSide.SOUTH])
-        case '-': return PipeTile([TileSide.WEST, TileSide.EAST])
-        case 'L': return PipeTile([TileSide.NORTH, TileSide.EAST])
-        case 'J': return PipeTile([TileSide.NORTH, TileSide.WEST])
-        case '7': return PipeTile([TileSide.WEST, TileSide.SOUTH])
-        case 'F': return PipeTile([TileSide.EAST, TileSide.SOUTH])
-        case '.': return GroundTile()
-        case 'S': return StartingTile()
+        case '|': return PipeTile([TileSide.NORTH, TileSide.SOUTH], position)
+        case '-': return PipeTile([TileSide.WEST, TileSide.EAST], position)
+        case 'L': return PipeTile([TileSide.NORTH, TileSide.EAST], position)
+        case 'J': return PipeTile([TileSide.NORTH, TileSide.WEST], position)
+        case '7': return PipeTile([TileSide.WEST, TileSide.SOUTH], position)
+        case 'F': return PipeTile([TileSide.EAST, TileSide.SOUTH], position)
+        case '.': return GroundTile(position)
+        case 'S': return StartingTile(position)
 
 def _parse_tile_graph(input_lines: list[str]) -> TileGraph:
     graph = TileGraph()
@@ -18,7 +18,7 @@ def _parse_tile_graph(input_lines: list[str]) -> TileGraph:
     for i, line in enumerate(input_lines):
         tiles_row: list[Tile] = []
         for j, tile_str in enumerate(line):
-            tile: Tile = _get_tile(tile_str)
+            tile: Tile = _get_tile(tile_str, (i, j))
             if isinstance(tile, StartingTile):
                 graph.starting_position = (i,j)
             tiles_row.append(tile)
@@ -26,19 +26,48 @@ def _parse_tile_graph(input_lines: list[str]) -> TileGraph:
 
     return graph
 
+def _update_connectors(connectors: list[TileSide], graph: TileGraph, offset: tuple[int, int], side: TileSide) -> None:
+    s_i = graph.starting_position[0]
+    s_j = graph.starting_position[1]
+    neighbouring_tile = graph.tiles[s_i + offset[0]][s_j + offset[1]]
+    assert isinstance(neighbouring_tile, PipeTile)
+    if neighbouring_tile.has(side): connectors.append(side.get_opposite())
+
 def _populate_starting_tile(graph: TileGraph) -> None:
     s_i = graph.starting_position[0]
     s_j = graph.starting_position[1]
     connectors = []
-    if graph.tiles[s_i - 1][s_j].has(TileSide.SOUTH): connectors.append(TileSide.NORTH)
-    if graph.tiles[s_i + 1][s_j].has(TileSide.NORTH): connectors.append(TileSide.SOUTH)
-    if graph.tiles[s_i][s_j - 1].has(TileSide.EAST): connectors.append(TileSide.WEST)
-    if graph.tiles[s_i][s_j + 1].has(TileSide.WEST): connectors.append(TileSide.EAST)
+    _update_connectors(connectors, graph, (-1, 0), TileSide.SOUTH)
+    _update_connectors(connectors, graph, (1, 0), TileSide.NORTH)
+    _update_connectors(connectors, graph, (0, -1), TileSide.EAST)
+    _update_connectors(connectors, graph, (0, 1), TileSide.WEST)
 
-    graph.tiles[s_i][s_j] = PipeTile(connectors)
+    graph.tiles[s_i][s_j] = PipeTile(connectors, s_i, s_j)
+
+def _get_pipe_tile(graph, i, j) -> PipeTile:
+    starting_tile = graph.tiles[i][j]
+    assert isinstance(starting_tile, PipeTile)
+    return starting_tile
+
+def _get_next_tile_in_loop(graph: TileGraph, current_tile: PipeTile, exiting_side: TileSide) -> tuple[PipeTile, TileSide]:
+    entering_side = exiting_side.get_opposite()
+    next_node_position = (current_tile.get_position()[0] + exiting_side.get_next_node_offset()[0],
+                          current_tile.get_position()[1] + exiting_side.get_next_node_offset()[1])
+    next_node = graph.tiles[next_node_position[0]][next_node_position[1]]
+    assert isinstance(next_node, PipeTile)
+    next_exiting_side = next_node.get_other_connector(entering_side)
+    return (next_node, next_exiting_side)
 
 def _count_tiles_in_loop(graph: TileGraph) -> int:
-    return 4
+    starting_tile = _get_pipe_tile(graph, graph.starting_position[0], graph.starting_position[1])
+    loop_length = 0
+
+    (current_tile, exiting_side) = _get_next_tile_in_loop(graph, starting_tile, starting_tile.connectors[0])
+    while current_tile is not starting_tile:
+        loop_length += 1
+        (current_tile, exiting_side) = _get_next_tile_in_loop(graph, current_tile, exiting_side)
+
+    return loop_length
 
 class PipeMaze(solution.AbstractSolution):
 
--- AdventOfCodePython/src/aoc2023/day10/tile_graph.py
+++ AdventOfCodePython/src/aoc2023/day10/tile_graph.py
@@ -1,25 +1,61 @@
-from abc import ABC, abstractmethod
-from enum import EnumType
+from abc import ABC
 
-class TileSide(EnumType): NORTH = 'N'; SOUTH = 'S'; WEST = 'W'; EAST = 'E'
+class TileSide:
+    NORTH: "TileSide"
+    SOUTH: "TileSide"
+    WEST: "TileSide"
+    EAST: "TileSide"
+
+    def __init__(self, next_node_offset: tuple[int, int]):
+        self.next_node_offset = next_node_offset
+        self.opposite: TileSide = None
+
+    @classmethod
+    def init_sides(cls):
+        cls.NORTH = TileSide((-1, 0))
+        cls.SOUTH = TileSide((1, 0))
+        cls.WEST = TileSide((0, -1))
+        cls.EAST = TileSide((0, 1))
+
+        cls.NORTH.opposite = cls.SOUTH
+        cls.SOUTH.opposite = cls.NORTH
+        cls.WEST.opposite = cls.EAST
+        cls.EAST.opposite = cls.WEST
+
+    def get_next_node_offset(self) -> tuple[int, int]:
+        return self.next_node_offset
+
+    def get_opposite(self):
+        return self.opposite
 
 class Tile(ABC):
-    @abstractmethod
-    def has(self, connector: TileSide) -> bool: pass
+
+    def __init__(self, position: tuple[int, int]):
+        super().__init__()
+        self.__position = position
+
+    def get_position(self) -> tuple[int, int]:
+        return self.__position
 
 class PipeTile(Tile):
-    def __init__(self, connectors: list[TileSide]):
-        super().__init__()
+    def __init__(self, connectors: list[TileSide], position: tuple[int, int]):
+        super().__init__(position)
         self.connectors = connectors
 
     def has(self, connector: TileSide) -> bool:
         return connector in self.connectors
 
+    def get_other_connector(self, side):
+        side_index = self.connectors.index(side)
+        return self.connectors[side_index + 1 % 2]
+
 class GroundTile(Tile):
-    def has(self, connector: TileSide) -> bool: return False
+    def __init__(self, position: tuple[int, int]):
+        super().__init__(position)
 
 class StartingTile(Tile):
-    def has(self, connector: TileSide) -> bool: raise NotImplemented
+    def __init__(self, position: tuple[int, int]):
+        super().__init__(position)
 
 class TileGraph:
     starting_position: tuple[int, int] = None
