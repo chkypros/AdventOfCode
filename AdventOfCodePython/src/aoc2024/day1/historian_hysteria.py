d370f01be2d479daf8508aa19d021f76a4950d49
Kypros Chrysanthou <chkypros@gmail.com>
2024-12-01 23:50:38 +0200

    AOP 2024 - Day 01 Setup

---
+++ AdventOfCodePython/src/aoc2024/day1/historian_hysteria.py
@@ -0,0 +1,4 @@
+import src.aoc_template.solution as solution
+
+class HistorianHysteria(solution.AbstractSolution):
+    pass
---
+++ AdventOfCodePython/test/aoc2024/day1/test_historian_hysteria.py
@@ -0,0 +1,6 @@
+import test.aoc_template.solution_test as solution_test
+
+from src.aoc2024.day1.historian_hysteria import HistorianHysteria
+
+class HistorianHysteriaTest(solution_test.AbstractSolutionTest):
+    solution = HistorianHysteria()
