ae7391e59ce87eb96ea332c2aca3bb5762800ad7
Kypros Chrysanthou <chkypros@gmail.com>
2024-12-02 13:52:00 +0200

    AOP 2024 - Day 02 Setup

---
+++ AdventOfCodePython/test/aoc2024/day2/test_red_nosed_reports.py
@@ -0,0 +1,7 @@
+import test.aoc_template.solution_test as solution_test
+
+from src.aoc2024.day2.red_nosed_reports import RedNosedReports
+
+class HistorianHysteriaTest(solution_test.AbstractSolutionTest):
+    EXPECTED_ANSWER_PART_ONE_SAMPLE = "2"
+    solution = RedNosedReports()
