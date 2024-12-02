671a990994f503eafcbdc0ce0653eaa72b1ac0c1
Kypros Chrysanthou <chkypros@gmail.com>
2024-12-02 08:32:32 +0200

    AOP 2024 - Day 01 Implemented part one

--- AdventOfCodePython/src/aoc2024/day1/historian_hysteria.py
+++ AdventOfCodePython/src/aoc2024/day1/historian_hysteria.py
@@ -1,4 +1,20 @@
 import src.aoc_template.solution as solution
 
+def _parse_lists(input_lines: list[str]) -> tuple[list[int], list[int]]:
+    left_list = []
+    right_list = []
+    for i in range(len(input_lines)):
+        parts = input_lines[i].split()
+        left_list.append(int(parts[0]))
+        right_list.append(int(parts[1]))
+    return left_list, right_list
+
 class HistorianHysteria(solution.AbstractSolution):
-    pass
+    def solve_part_one(self, input_lines: list[str]) -> object:
+        diff = 0
+        left_list, right_list = _parse_lists(input_lines)
+        sorted_left_list = sorted(left_list)
+        sorted_right_list = sorted(right_list)
+        for i in range(len(sorted_left_list)):
+            diff += abs(sorted_left_list[i] - sorted_right_list[i])
+        return diff
--- AdventOfCodePython/test/aoc2024/day1/test_historian_hysteria.py
+++ AdventOfCodePython/test/aoc2024/day1/test_historian_hysteria.py
@@ -3,4 +3,6 @@ import test.aoc_template.solution_test as solution_test
 from src.aoc2024.day1.historian_hysteria import HistorianHysteria
 
 class HistorianHysteriaTest(solution_test.AbstractSolutionTest):
+    EXPECTED_ANSWER_PART_ONE_SAMPLE = "11"
+    EXPECTED_ANSWER_PART_ONE = "3569916"
     solution = HistorianHysteria()
