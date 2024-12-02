205a0ac1607363acc41e9e3d26ed99148b5967f4
Kypros Chrysanthou <chkypros@gmail.com>
2024-12-02 08:50:16 +0200

    AOP 2024 - Day 01 Implemented part two

--- AdventOfCodePython/src/aoc2024/day1/historian_hysteria.py
+++ AdventOfCodePython/src/aoc2024/day1/historian_hysteria.py
@@ -1,4 +1,5 @@
 import src.aoc_template.solution as solution
+from collections import Counter
 
 def _parse_lists(input_lines: list[str]) -> tuple[list[int], list[int]]:
     left_list = []
@@ -18,3 +19,10 @@ class HistorianHysteria(solution.AbstractSolution):
         for i in range(len(sorted_left_list)):
             diff += abs(sorted_left_list[i] - sorted_right_list[i])
         return diff
+
+    def solve_part_two(self, input_lines: list[str]) -> object:
+        left_list, right_list = _parse_lists(input_lines)
+        left_count_map = Counter(left_list)
+        right_count_map = Counter(right_list)
+        scores = [num * left_count_map[num] * right_count_map.get(num, 0) for num in left_count_map.keys()]
+        return sum(scores)
--- AdventOfCodePython/test/aoc2024/day1/test_historian_hysteria.py
+++ AdventOfCodePython/test/aoc2024/day1/test_historian_hysteria.py
@@ -5,4 +5,6 @@ from src.aoc2024.day1.historian_hysteria import HistorianHysteria
 class HistorianHysteriaTest(solution_test.AbstractSolutionTest):
     EXPECTED_ANSWER_PART_ONE_SAMPLE = "11"
     EXPECTED_ANSWER_PART_ONE = "3569916"
+    EXPECTED_ANSWER_PART_TWO_SAMPLE = "31"
+    EXPECTED_ANSWER_PART_TWO = "0"
     solution = HistorianHysteria()
