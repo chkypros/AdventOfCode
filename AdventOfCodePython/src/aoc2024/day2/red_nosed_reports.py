d2375b9aa3dd67afc98b4b9c9ca23a905ecb1920
Kypros Chrysanthou <chkypros@gmail.com>
2024-12-03 22:50:53 +0200

    AOP 2024 - Day 02 Part two WIP

--- AdventOfCodePython/src/aoc2024/day2/red_nosed_reports.py
+++ AdventOfCodePython/src/aoc2024/day2/red_nosed_reports.py
@@ -15,7 +15,50 @@ def _report_is_safe(report: str) -> bool:
         if not (step_filter(levels[i-1], levels[i])): return False
     return True
 
+def _report_is_safe_with_dampener(report: str) -> bool:
+    levels = list(map(int, report.split()))
+    dampened_index = None
+
+    num_levels = len(levels)
+    mid_level_index = int(num_levels / 2)
+    is_first_step_incrementing = levels[0] < levels[1]
+    is_middle_step_incrementing = levels[mid_level_index - 1] < levels[mid_level_index]
+    is_last_step_incrementing = levels[num_levels - 2] < levels[num_levels - 1]
+
+    if is_middle_step_incrementing == is_last_step_incrementing != is_first_step_incrementing:
+        dampened_index = 0
+        previous = levels[1]
+        i = 2
+        if is_middle_step_incrementing:
+            step_filter = _increasing_safe_step_filter
+        else:
+            step_filter = _decreasing_safe_step_filter
+    else:
+        i = 1
+        previous = levels[0]
+        if is_first_step_incrementing:
+            step_filter = _increasing_safe_step_filter
+        else:
+            step_filter = _decreasing_safe_step_filter
+
+    while i < num_levels:
+        if not step_filter(previous, levels[i]):
+            if dampened_index is None:
+                dampened_index = i
+            else:
+                return False
+        else:
+            previous = levels[i]
+
+        i += 1
+
+    return True
+
 class RedNosedReports(solution.AbstractSolution):
     def solve_part_one(self, input_lines: list[str]) -> object:
         safe_reports = [report for report in input_lines if _report_is_safe(report)]
+        return len(safe_reports)
+
+    def solve_part_two(self, input_lines: list[str]) -> object:
+        safe_reports = [report for report in input_lines if _report_is_safe_with_dampener(report)]
         return len(safe_reports)
--- AdventOfCodePython/test/aoc2024/day2/test_red_nosed_reports.py
+++ AdventOfCodePython/test/aoc2024/day2/test_red_nosed_reports.py
@@ -5,4 +5,6 @@ from src.aoc2024.day2.red_nosed_reports import RedNosedReports
 class HistorianHysteriaTest(solution_test.AbstractSolutionTest):
     EXPECTED_ANSWER_PART_ONE_SAMPLE = "2"
     EXPECTED_ANSWER_PART_ONE = "407"
+    EXPECTED_ANSWER_PART_TWO_SAMPLE = "4"
+    EXPECTED_ANSWER_PART_TWO = "0"
     solution = RedNosedReports()
