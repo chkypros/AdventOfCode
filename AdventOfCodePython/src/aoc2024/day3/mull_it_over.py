9db465c7b3ad411a4f017a909198262c09f69be8
Kypros Chrysanthou <chkypros@gmail.com>
2024-12-03 23:15:48 +0200

    AOP 2024 - Day 03 Implemented part one

--- AdventOfCodePython/src/aoc2024/day3/mull_it_over.py
+++ AdventOfCodePython/src/aoc2024/day3/mull_it_over.py
@@ -1,5 +1,12 @@
+import re
+
 import src.aoc_template.solution as solution
 
 class MullItOver(solution.AbstractSolution):
     def solve_part_one(self, input_lines: list[str]) -> object:
-        pass
+        products = [
+            int(match[0]) * int(match[1])
+            for i in range(len(input_lines))
+            for match in re.findall("mul\((\d+),(\d+)\)", input_lines[i])
+        ]
+        return sum(products)
--- AdventOfCodePython/test/aoc2024/day3/test_mull_it_over.py
+++ AdventOfCodePython/test/aoc2024/day3/test_mull_it_over.py
@@ -4,4 +4,5 @@ from src.aoc2024.day3.mull_it_over import MullItOver
 
 class MullItOverTest(solution_test.AbstractSolutionTest):
     EXPECTED_ANSWER_PART_ONE_SAMPLE = "161"
+    EXPECTED_ANSWER_PART_ONE = "183788984"
     solution = MullItOver()
