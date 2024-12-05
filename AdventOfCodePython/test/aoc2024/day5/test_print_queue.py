aca0f1abc041f54768f18b7a2a0f01ffa3edcbd1
Kypros Chrysanthou <chkypros@gmail.com>
2024-12-05 23:53:05 +0200

    AOP 2024 - Day 05 Setup

---
+++ AdventOfCodePython/test/aoc2024/day5/test_print_queue.py
@@ -0,0 +1,7 @@
+import test.aoc_template.solution_test as solution_test
+
+from src.aoc2024.day5.print_queue import PrintQueue
+
+class CeresSearchTest(solution_test.AbstractSolutionTest):
+    EXPECTED_ANSWER_PART_ONE_SAMPLE = "143"
+    solution = PrintQueue()
